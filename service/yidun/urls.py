6389de7d813ebd91360b287fa4633ef83cb5caca
Walkerrrrrrrrrr <552345133@qq.com>
2024-09-06 18:04:25 +0800

    修复易盾错误

--- service/yidun/urls.py
+++ service/yidun/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from fastapi import APIRouter
 
-router = APIRouter(prefix='/xhs')
+router = APIRouter(prefix='/yidun')
 
 router.add_api_route('/sign', views.sign, methods=['POST'])
